d0539ee67ff57efe341ea46d6fcbaaab4c4bc21c
jorgeMontoya022 <142169854+jorgeMontoya022@users.noreply.github.com>
2025-05-01 16:16:01 -0500

    initalCommit

---
+++ monedero_virutal/src/main/java/co/edu/uniquindio/monedero_virtual/PrimaryController.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.monedero_virtual;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ monedero_virutal/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module co.edu.uniquindio.monedero_virtual {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens co.edu.uniquindio.monedero_virtual to javafx.fxml;
+    exports co.edu.uniquindio.monedero_virtual;
+}
